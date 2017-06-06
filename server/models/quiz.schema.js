83bcc8c93a0a293de0eb705da37892423c44032e
rbnrjR <rubanraj.rj@gmail.com>
2017-06-06 11:42:52 +0530

    Schema for Quiz

---
+++ server/models/quiz.schema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose"),
+	    Schema=mongoose.Schema;
+
+
+var quizSchema=new Schema({
+    topic:String,
+    subtopic:String,
+    hostedBy:String,
+    date:Date,
+    startTime:Date,
+    endTime:Date,
+    questions:[{question:String,options:[String],correctAnswer:String}]
+});
+
+
+var quiz=mongoose.model("quiz",quizSchema);
+
+module.exports=quiz;
