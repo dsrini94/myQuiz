30e21432383c98e6b38e3f0a55cb935abb5f4586
rbnrjR <rubanraj.rj@gmail.com>
2017-06-07 11:02:39 +0530

    date assigning bug rectified while launching

--- server/routes/launchQuiz.routes.js
+++ server/routes/launchQuiz.routes.js
@@ -10,30 +10,41 @@ launchQuiz.post('/saveLaunchQuiz', function(req, res){
     , startDate = req.body.startDate
     , startTime = req.body.startTime
     , endTime = req.body.endTime
-    , questions = req.body.questions;
-  console.log(questions);
+    , questions = req.body.questions
+    , d = ''
+    , m = ''
+    , y = '';
+
+  //Shuffling the options array ---------->
   questions.map(function(item,i){
     item.options = shuffle(item.options);
   });
-  var obj = {
-    topic:topic,
-    subtopic:subtopic,
-    hostedBy:hostedBy,
-    date:startDate,
-    startTime:startTime,
-    endTime:endTime,
-    questions:questions
-  }
-  console.log(questions);
-  QuizSchema.update({topic:topic,subtopic:subtopic,hostedBy:hostedBy},
+
+  //setting the chosen date to the start and end time ---------->
+  var a = new Date(startDate);
+  d = a.getDate();
+  m = a.getMonth();
+  y = a.getFullYear();
+  var st = new Date(startTime);
+  st.setDate(d);
+  st.setMonth(m);
+  st.setFullYear(y);
+  var et = new Date(endTime);
+  et.setDate(d);
+  et.setMonth(m);
+  et.setFullYear(y);
+
+  //Query to save launched quiz ---------->
+  QuizSchema.update({topic:topic,subtopic:subtopic,hostedBy:hostedBy,date:startDate},
                     {$set:{topic:topic,
                             subtopic:subtopic,
                             hostedBy:hostedBy,
                             date:startDate,
-                            startTime:startTime,
-                            endTime:endTime,
+                            startTime:st,
+                            endTime:et,
                             questions:questions}},
-                    {upsert:true}, function(err, reply){
+                    {upsert:true},
+  function(err, reply){
     if (err) {
       console.log('Oops! Error in saving quiz to DB', err);
     } else {
@@ -41,8 +52,6 @@ launchQuiz.post('/saveLaunchQuiz', function(req, res){
     }
   });
 });
-
-
 
 
 // function to shuffle the option order ---------->
