ee787c5e959f7cd5a80c59871b8fd07d134a5738
rbnrjR <rubanraj.rj@gmail.com>
2017-06-10 16:46:51 +0530

    event organised as per start time

--- server/routes/events.route.js
+++ server/routes/events.route.js
@@ -7,6 +7,7 @@ events.get('/events', function(req, res){
     eventsArr : []
   };
   QuizSchema.find({}, function(err, reply){
+    console.log('res - > ',reply);
     if (err) {
       // console.log('Error in Retrieving the events in {events.route.js} ',err);
     } else {
@@ -24,8 +25,32 @@ function eventSegregation(val){
   var current = new Date();
   var eArr = [];
   val.map(function(item, i){
-    if(item.date>=current){
+    var a=new Date(item.date);
+    date = a.getDate();
+    month=a.getMonth();
+    year=a.getFullYear();
+    // Quiz Start Time
+    var st=new Date(item.startTime);
+    st.setDate(date);
+    st.setMonth(month);
+    st.setFullYear(year);
+    // Quiz End Time
+    var et = new Date(item.endTime);
+    et.setDate(date);
+    et.setMonth(month);
+    et.setFullYear(year);
+    // Date validation variables
+    var m = st.getMonth()+1;
+    var stt = st.getDate()+'/'+m+'/'+st.getFullYear();
+    var mmm = current.getMonth()+1;
+    var currentDate = current.getDate()+'/'+mmm+'/'+current.getFullYear()
+    console.log('date : ',stt);
+    console.log('current : ',currentDate);
+
+    if(stt>=currentDate){
       obj = {
+        st : st,
+        et : et,
         date : item.date,
         topic : item.topic,
         subtopic : item.subtopic,
